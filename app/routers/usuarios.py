bfd43472b23c8698e084361b3c3259b4d500d999
Grazielly Souza <graziellysouza2580@gmail.com>
2026-07-13 22:19:36 -0300

    add router usuarios

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -1,12 +1,55 @@
-# novo_usuario = Usuario(
-#     nome=usuario.nome,
-#     matricula=usuario.matricula,
-#     cargo=usuario.cargo,
-#     username=usuario.username,
-#     email=usuario.email,
-#     senha=senha_hash
-# )
+from fastapi import APIRouter, HTTPException
 
-# db.add(novo_usuario)
-# await db.commit()
-# await db.refresh(novo_usuario)
+import crud
+import schemas
+
+router = APIRouter(
+    prefix="/usuarios",
+    tags=["Usuários"]
+)
+
+@router.post("/", response_model=schemas.UsuarioResponse)
+async def criar_usuario(usuario: schemas.UsuarioCreate):
+    return await crud.criar_usuario(usuario)
+
+@router.get("/", response_model=list[schemas.UsuarioResponse])
+async def listar_usuarios():
+    return await crud.listar_usuarios()
+
+@router.get("/{id}", response_model=schemas.UsuarioResponse)
+async def buscar_usuario(id: int):
+    usuario = await crud.buscar_usuario(id)
+
+    if usuario is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuário não encontrado."
+        )
+    
+    return usuario
+
+@router.put("/{id}", response_model=schemas.UsuarioResponse)
+async def atualizar_usuario(id: int, usuario: schemas.UsuarioCreate):
+    usuario_atualizado = await crud.atualizar_usuario(id, usuario)
+
+    if usuario_atualizado is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuário não encontrado."
+        )
+    
+    return usuario_atualizado
+
+@router.delete("/{id}")
+async def deletar_usuario(id: int):
+    usuario = await crud.deletar_usuario(id)
+
+    if usuario is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuário não encontrado."
+        )
+    
+    return {
+        "message": "Usuário deletado com sucesso!"
+    }
